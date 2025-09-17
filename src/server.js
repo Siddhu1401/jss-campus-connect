7b16d79dd2e5f2d6850eecfae7897e8e6d56d207
Siddhant Mishra <mishrasid1401@gmail.com>
2025-09-17 11:25:30 +0530

    edited server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,17 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const pool = require('./config/db');
 
 app.use(express.json());
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('DATABASE CONNECTION FAILED:', err.stack);
+    } else {
+        console.log('Database connected successfully.');
+    }
+  });    
 
 const PORT = process.env.PORT || 5000;
 
