71907896b7bef2e1890a0ccdedb36cf66a7fe4d4
Siddhant Mishra <mishrasid1401@gmail.com>
2025-10-11 19:23:26 +0530

    debugs and additions

--- src/api/user.routes.js
+++ src/api/user.routes.js
@@ -1,11 +1,9 @@
 // src/api/user.routes.js
 const express = require('express');
 const router = express.Router();
-
-
 const { registerUser } = require('../controllers/user.controller.js');
 
-
+// This connects the /register path to our controller logic
 router.post('/register', registerUser);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,34 +1,31 @@
-
+// server.js
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const pool = require('./config/db');
 
+// This allows our app to accept JSON data from the client
 app.use(express.json());
 
+// This block confirms our database is connected on startup
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
         console.error('DATABASE CONNECTION FAILED:', err.stack);
     } else {
         console.log('Database connected successfully.');
     }
-  });    
+});
 
 const PORT = process.env.PORT || 5000;
 
-
+// A simple route to check if the API is running
 app.get('/', (req, res) => {
-  res.send('JSS Campus Connect API is running...');
+    res.send('JSS Campus Connect API is running...');
 });
 
-app.post('/api/users/register', (req, res) => {
-    console.log('--- TEST ROUTE WAS HIT ---');
-    res.json({ message: 'Success! The test route in server.js works.' });
-});
-//app.use('/api/users', require('./api/user.routes.js'));
-
+// This line connects all our user-related routes
+app.use('/api/users', require('./api/user.routes.js'));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
