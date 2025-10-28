a481305bf750ded3d4e893df4a27d5f53425563b
Siddhant Mishra <mishrasid1401@gmail.com>
2025-10-28 20:40:46 +0530

    delete controller added

--- src/controllers/resource.controller.js
+++ src/controllers/resource.controller.js
@@ -74,8 +74,46 @@ const getResourceById = async (req, res) => {
     }
 };
 
+const deleteResource = async (req, res) => {
+    try {
+        const resourceId = req.params.id;
+        const userId = req.user.id; 
+        
+        const resource = await pool.query(
+            'SELECT user_id FROM resources WHERE resource_id = $1',
+            [resourceId]
+        );
+
+        
+        if (resource.rows.length === 0) {
+            return res.status(404).json({ msg: 'Resource not found' });
+        }
+
+        const resourceOwnerId = resource.rows[0].user_id;
+
+        if (resourceOwnerId !== userId) {
+            return res.status(401).json({ msg: 'User not authorized to delete this resource' });
+        }
+
+        await pool.query('DELETE FROM resources WHERE resource_id = $1', [resourceId]);
+
+        res.json({ msg: 'Resource removed successfully' });
+
+        } 
+       
+        catch (err) {
+        console.error(err.message);
+        
+        if (err.code === '22P02') {
+             return res.status(404).json({ msg: 'Resource not found (invalid ID)' });
+        }
+        res.status(500).send('Server Error');
+    }
+};
+
 module.exports = {
     createResource,
     getAllResources,
     getResourceById,
+    deleteResource,
 };
